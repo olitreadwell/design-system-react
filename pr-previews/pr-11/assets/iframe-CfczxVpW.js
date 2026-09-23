daac7a8f985805c7d0473bf8a4ac1aa65401af40
olitreadwell <olitreadwell@users.noreply.github.com>
2026-09-23 23:46:30 +0000

    Deploy preview for PR 11 🛫

---
+++ pr-previews/pr-11/assets/iframe-CfczxVpW.js
@@ -0,0 +1,1 @@
+import{t as e}from"./iframe-bI81RRVR.js";e();
---
+++ pr-previews/pr-11/src/types/width-percent.d.ts
@@ -0,0 +1,5 @@
+/**
+ * Values pulled from cfpb-core width utilities
+ * https://github.com/cfpb/design-system/blob/main/packages/cfpb-design-system/src/utilities/utilities.scss#L189-L230
+ */
+export type WidthPercent = '10' | '20' | '25' | '30' | '33' | '40' | '50' | '60' | '66' | '70' | '75' | '80' | '90' | '100' | 10 | 20 | 25 | 30 | 33 | 40 | 50 | 60 | 66 | 70 | 75 | 80 | 90 | 100;
